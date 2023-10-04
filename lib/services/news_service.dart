1ad22088cfe141efc3847eeeebed4bde7a8e8147
Hishamk99 <hishamkamel370@gmail.com>
2023-10-04 07:41:27 +0200

    using dio package & create responce object

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,12 +1,17 @@
 import 'package:dio/dio.dart';
-class NewsService
-{
+
+class NewsService {
   //final dio = Dio();
   final Dio dio;
   NewsService(this.dio);
 
-  void getHttp() async {
-    final response = await dio.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=9c80b0f865784b458b6f6545e07de057');
-    print(response);
-}
+  void getNews() async {
+    Response response = await dio.get(
+        'https://newsapi.org/v2/top-headlines?country=eg&category=general&apiKey=9c80b0f865784b458b6f6545e07de057');
+
+    Map<String, dynamic> jsonData = response.data;
+    List<Map<String, dynamic>> articles =
+        jsonData['articles'] as List<Map<String, dynamic>>;
+    // == List<dynamic> articles =  jsonData['articles'];
+  }
 }
