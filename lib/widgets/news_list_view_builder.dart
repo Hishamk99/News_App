662604e1454ee7ffc41c6bf15d8a5393d12b134f
Hishamk99 <hishamkamel370@gmail.com>
2023-10-05 13:55:11 +0200

    if condition in FutureBuilder widget

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/services/news_service.dart';
-
 import 'news_list_view.dart';
 
 class NewsListViewBuilder extends StatelessWidget {
@@ -11,12 +10,17 @@ class NewsListViewBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: NewsService(Dio()).getNews(),
-      builder: (context, snapshot) {
-      return NewsListView(
-        articleList: snapshot.data ?? []
-      );
-    });
+        future: NewsService(Dio()).getNews(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return NewsListView(articleList: snapshot.data ?? []);
+          }
+          else if(snapshot.hasError)
+          {
+            return const SliverToBoxAdapter(child: Center(child: Text('There was an error')));
+          }
+          return const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+        });
     // return isLoading
     //     ? const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()))
     //     :articles.isNotEmpty ? NewsListView(
